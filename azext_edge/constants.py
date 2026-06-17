b7faa69200b40ff2a81ee78c742b7163236ad0b6
Ketki Naik <56810359+ketkimnaik@users.noreply.github.com>
2026-06-17 09:59:39 -0700

    build: version increments for 2606 release (#888)
    * Make release ready
    
    * Update the version
    
    ---------
    
    Co-authored-by: Ketki <ketkinaik@microsoft.com>

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -11,4 +11,4 @@ VERSION = "2.7.0a2"
 EXTENSION_NAME = "azure-iot-ops"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "IotOperationsCliExtension/{}".format(VERSION)
-AIO_RELEASE = "2605"
+AIO_RELEASE = "2606"
