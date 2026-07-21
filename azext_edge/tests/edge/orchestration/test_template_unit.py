f6a81a9239728e1de19dede7d4a3e7178bea2300
Yuelin Zhao <yuelinzhao@microsoft.com>
2026-07-21 23:47:16 +0000

    set AIO extention to stable, bump CLI version

--- azext_edge/tests/edge/orchestration/test_template_unit.py
+++ azext_edge/tests/edge/orchestration/test_template_unit.py
@@ -154,7 +154,7 @@ EXTENSION_CONFIGS = {
         (EXTENSION_TYPE_SSC, "1.5.1", "stable"),
     ],
     "instance": [
-        (EXTENSION_TYPE_OPS, "1.4.41", "integration"),
+        (EXTENSION_TYPE_OPS, "1.4.41", "stable"),
     ],
 }
 
